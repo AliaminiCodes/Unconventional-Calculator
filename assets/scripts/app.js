9eb88264b71e22d461dbe0e15b620e53a0b582d7
Ali Amini <aliamini.codes@gmail.com>
2025-02-16 07:48:55 +0330

    Now we have calcDescription

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,9 @@ const defultResult = 0;
 let currentResult = defultResult;
 
 function add() {
+    const calcDescription = `${currentResult} + ${userInput.value}`;
     currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, "");
+    outputResult(currentResult,calcDescription );
 }
 
 
