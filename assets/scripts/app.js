5c81e56e153a85c7a49a2a0ced780808c036f1e6
Ali Amini <aliamini.codes@gmail.com>
2025-02-17 08:43:01 +0330

    Graet! for ( add & subtract  ) but we need to "Refactoring" process

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,27 @@
 const defultResult = 0;
 let currentResult = defultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
 function add() {
-    const calcDescription = `${currentResult} + ${userInput.value}`;
-    currentResult = currentResult + parseInt(userInput.value);
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
     outputResult(currentResult,calcDescription );
 }
 
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult,calcDescription );
+}
 
-
+subtractBtn.addEventListener("click", subtract);
 addBtn.addEventListener("click", add);
  
-console.log(typeof add);
+
+
+console.log("");
