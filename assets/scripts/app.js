02766032dcea0c483d6c6c8a810dd94444ba95d5
Ali Amini <aliamini.codes@gmail.com>
2025-02-18 07:37:14 +0330

    Refactoring is done!

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,24 +4,44 @@ let currentResult = defultResult;
 function getUserNumberInput() {
     return parseInt(userInput.value);
 }
+function creatAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult,calcDescription );
+}
 
 function add() {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    const iniitialResult = currentResult;
     currentResult = currentResult + enteredNumber;
-    outputResult(currentResult,calcDescription );
+    creatAndWriteOutput('+', iniitialResult, enteredNumber);
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    const iniitialResult = currentResult;
     currentResult = currentResult - enteredNumber;
-    outputResult(currentResult,calcDescription );
+    creatAndWriteOutput('-', iniitialResult, enteredNumber);
 }
 
-subtractBtn.addEventListener("click", subtract);
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const iniitialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    creatAndWriteOutput('*', iniitialResult, enteredNumber);
+}
+
+function divide () {
+    const enteredNumber = getUserNumberInput();
+    const iniitialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    creatAndWriteOutput('/', iniitialResult, enteredNumber);    
+}
+
+
 addBtn.addEventListener("click", add);
- 
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 
 
 console.log("");
