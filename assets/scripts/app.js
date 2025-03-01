fdc21b67c40d117cec4c07d224ce92128195d863
Ali Amini <aliamini.codes@gmail.com>
2025-03-02 03:18:32 +0330

    Now, with this clever trick, instead of just filtering 0, I filtered out another Falsy value for every entered value! (like undefined and null etc)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -36,7 +36,7 @@ function calculateResult(calculationType) {
         calculationType !== 'SUBTRACT' &&
         calculationType !== 'MULTIPLY' &&
         calculationType !== 'DIVIDE' ||
-        enteredNumber === 0
+        !enteredNumber 
     ) {
         return;
     }
