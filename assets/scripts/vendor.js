d04a38226df2c74e17828a5990cdd828485cb876
Ali Amini <aliamini.codes@gmail.com>
2025-03-02 18:33:36 +0330

    added "PERCENT" button (vendor.js)

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -5,6 +5,7 @@ const multiplyBtn = document.getElementById("btn-multiply");
 const divideBtn = document.getElementById("btn-divide");
 const clearBtn = document.getElementById("btn-clear");
 const powerBtn = document.getElementById("btn-power");
+const percentBtn = document.getElementById("btn-percent");
 
 const currentResultOutput = document.getElementById("current-result");
 const currentCalculationOutput = document.getElementById("current-calculation");
